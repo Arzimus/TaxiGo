9b0cacc11b81168c74cb23482ca8889f2ae34f9d
Rohit <rohitgotey@gmail.com>
2024-07-07 12:30:27 +0530

    updated the Booking section

--- app/api/search-address/route.ts
+++ app/api/search-address/route.ts
@@ -1,3 +1,4 @@
+
 import { NextResponse } from "next/server";
 const BASE_URL = "https://api.mapbox.com/search/searchbox/v1/suggest"
 export async function GET(request: any) {
@@ -6,7 +7,7 @@ export async function GET(request: any) {
 
   const searchText = searchParams.get('q');
 
-  const res = await fetch(BASE_URL + '?q=' + searchText + '?language=en&limit=6&session_token=5ccce4a4-ab0a-4a7c-943d-580e55542363&country=IN'
+  const res = await fetch(BASE_URL + '?q=' + searchText + '?language=en&limit=6&session_token=5ccce4a4-ab0a-4a7c-943d-580e55542363'
     + "&access_token=" + process.env.MAPBOX_ACCESS_TOKEN,
     {
       headers: {
@@ -15,6 +16,6 @@ export async function GET(request: any) {
     })
 
   const searchResult = await res.json();
-  return Response.json(searchResult)
+  return NextResponse.json(searchResult)
 
 }
